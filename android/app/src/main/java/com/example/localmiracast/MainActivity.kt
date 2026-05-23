fcca3ef7fc7809ad9de10db075830903b7f0cdb2
jp <jphthird84@gmail.com>
2026-05-23 01:19:57 -0500

    uodate

--- android/app/src/main/java/com/example/localmiracast/MainActivity.kt
+++ android/app/src/main/java/com/example/localmiracast/MainActivity.kt
@@ -6,6 +6,6 @@ import android.os.Bundle
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        // Shizuku support will be implemented here
+        setContentView(R.layout.activity_main)
     }
 }
