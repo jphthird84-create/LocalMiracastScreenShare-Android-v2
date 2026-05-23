6c88dba935b49c97c283f328f9f5477253c7b12a
jp <jphthird84@gmail.com>
2026-05-23 01:04:08 -0500

    uodate

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -28,6 +28,16 @@ android {
     }
 }
 
+dependencies {
+    implementation(libs.core.ktx)
+    implementation(libs.appcompat)
+    implementation(libs.material)
+    implementation(libs.constraintlayout)
+    implementation(libs.shizuku.api)
+    implementation(libs.shizuku.provider)
+    testImplementation(libs.junit)
+}
+
 kotlin {
     compilerOptions {
         jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
---
+++ android/app/src/main/kotlin/com/example/localmiracast/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.localmiracast
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // Shizuku support will be implemented here
+    }
+}
